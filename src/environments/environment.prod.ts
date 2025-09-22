1d5a03d15fd44d83095e59155c70b5264ac217a1
pink0328-hash <pink.king0328@gmail.com>
2025-09-22 23:30:28 +0200

    update_config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDWqUNcYPMrFvHIpR48bIjDqWO20WiEBNw",
+    authDomain: "ionicangular-70ada.firebaseapp.com",
+    projectId: "ionicangular-70ada",
+    storageBucket: "ionicangular-70ada.firebasestorage.app",
+    messagingSenderId: "629254959303",
+    appId: "1:629254959303:web:4aafa5cc955c5f51f7cee2"
+  }
 };
